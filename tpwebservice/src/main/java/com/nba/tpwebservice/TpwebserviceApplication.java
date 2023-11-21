9b4f22ba650e4e7c59ba09dd6a69d7d484574372
01KingMaker <fabrakoto0987@gmail.com>
2023-11-21 10:16:49 +0300

    initial commit

---
+++ tpwebservice/src/main/java/com/nba/tpwebservice/TpwebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nba.tpwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpwebserviceApplication.class, args);
+	}
+
+}
---
+++ tpwebservice/src/test/java/com/nba/tpwebservice/TpwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba.tpwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
